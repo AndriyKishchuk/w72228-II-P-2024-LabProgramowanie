a54bbb9cbe2ac895c0ede2c90a5f4fa87eedf804
w72228 <andriy302007@gmail.com>
2025-05-13 21:35:01 +0200

    main

--- Lab5js.js
+++ Lab5js.js
@@ -31,34 +31,34 @@ convertCelsiusToFahrenheit();
 function arrayOperations() {
     const numbers = [5, 12, 8, 21, 7, 3, 18];
 
-    // Zsumuj wartości
+    // Suma
     const sum = numbers.reduce((acc, num) => acc + num, 0);
     console.log("Suma wartości:", sum);
 
-    // Znajdź liczby parzyste
+    // Parzysta
     const parzyste = numbers.filter(num => num % 2 === 0);
     console.log("Liczby parzyste:", parzyste);
 
-    // Pomnóż wartości razy 3
+    // Pomnóż na 3
     const Tree = numbers.map(num => num * 3);
     console.log("Wartości pomnożone razy 3:", Tree);
 
-    // Dodaj do tablicy swój numer albumu i znajdź jego index
+    // numer albumu i index
     const albumNumber = 72228;
     numbers.push(albumNumber);
     console.log("Tablica po dodaniu numeru albumu:", numbers);
     const albumIndex = numbers.indexOf(albumNumber);
     console.log("Index numeru albumu:", albumIndex);
 
-    // Oblicz średnią arytmetyczną
+    // średnią arytmetyczną
     const average = sum / numbers.length;
     console.log("Średnia arytmetyczna:", average);
 
-    // Znajdź największą liczbę
+    // największą liczbę
     const maxNumber = Math.max(...numbers);
     console.log("Największa liczba:", maxNumber);
 
-    // Zlicz ilość wystąpień wybranej wartości
+    // ilość wystąpień 8
     const value = 8;
     const liczba = numbers.filter(num => num === value).length;
     console.log(`Ilość wystąpień wartości ${value}:`, liczba);
